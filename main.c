52bc9c0e63ebb5d955c83f7ed740e843c9f31654
DoDo6b <katynskii.kiu@phystech.edu>
2025-07-25 02:18:47 +0300

    Nothing new

--- main.c
+++ main.c
@@ -3,10 +3,11 @@
 int main(){
     chunkDump(&allocated, "Allocated");
     chunkDump(&freed, "Freed");
-    char* ptr[1023];
+    char* ptr[1024];
     for(int i=0; i<CHUNKCAP; i++){
         ptr[i] = (char*)memalloc(sizeof(char));
     }
+    memfree(ptr[1023]);
     chunkDump(&allocated, "Allocated");
     chunkDump(&freed, "Freed");
     return 0;
